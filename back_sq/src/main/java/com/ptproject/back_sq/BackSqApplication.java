300275f964a77b4895dc8637def3d3509ed4f3b3
Heemin2be <heemin7904@gmail.com>
2025-11-15 20:42:18 +0900

    Initial commit

---
+++ back_sq/src/main/java/com/ptproject/back_sq/BackSqApplication.java
@@ -0,0 +1,13 @@
+package com.ptproject.back_sq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackSqApplication.class, args);
+    }
+
+}
---
+++ back_sq/src/test/java/com/ptproject/back_sq/BackSqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptproject.back_sq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
